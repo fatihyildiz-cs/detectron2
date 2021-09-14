1e59eb9f7ad56f9e73906f22aab79903a252f375
Fatih <62537806+fatihyildiz-cs@users.noreply.github.com>
2021-09-14 13:50:00 +0300

    Update __init__.py

--- projects/PanopticFCN_cityscapes/panopticfcn/__init__.py
+++ projects/PanopticFCN_cityscapes/panopticfcn/__init__.py
@@ -1,4 +1,3 @@
 from .config import add_panopticfcn_config
 from .panoptic_seg import PanopticFCN
 from .build_solver import build_lr_scheduler
-from ..data.cityscapes.cityscapes_panoptic_separated import register_all_cityscapes_panoptic
