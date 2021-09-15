73a7bd71b968cf11b9a5466da16470efc73f5d93
Fatih <62537806+fatihyildiz-cs@users.noreply.github.com>
2021-09-15 16:21:51 +0300

    Update visualizer.py

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -238,7 +238,7 @@ def _create_text_labels(classes, scores, class_names, is_crowd=None):
     labels = None
     if classes is not None:
         if class_names is not None and len(class_names) > 0:
-            labels = [class_names[i] for i in classes]
+            labels = [class_names[i-11] for i in classes]
         else:
             labels = [str(i) for i in classes]
     if scores is not None:
