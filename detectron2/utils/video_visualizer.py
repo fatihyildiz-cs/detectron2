f6191bf16589ebf95be04351b9ac0209276701a1
Fatih <62537806+fatihyildiz-cs@users.noreply.github.com>
2021-09-15 16:21:10 +0300

    Update video_visualizer.py

--- detectron2/utils/video_visualizer.py
+++ detectron2/utils/video_visualizer.py
@@ -166,8 +166,8 @@ class VideoVisualizer:
             for i in range(num_instances)
         ]
         colors = self._assign_colors(detected)
-#         labels = [self.metadata.thing_classes[k] for k in category_ids]
-        labels = ["undef" for k in category_ids]
+        labels = [self.metadata.thing_classes[k-11] for k in category_ids]
+#         labels = ["undef" for k in category_ids]
 
         frame_visualizer.overlay_instances(
             boxes=None,
