16ad02af747eb3ec00e690e028aa740265c2d863
Fatih <62537806+fatihyildiz-cs@users.noreply.github.com>
2021-09-19 17:52:30 +0300

    Update builtin_meta.py

--- detectron2/data/datasets/builtin_meta.py
+++ detectron2/data/datasets/builtin_meta.py
@@ -202,26 +202,54 @@ KEYPOINT_CONNECTION_RULES = [
 
 # All Cityscapes categories, together with their nice-looking visualization colors
 # It's from https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/helpers/labels.py  # noqa
+# CITYSCAPES_CATEGORIES = [
+#     {"color": (128, 64, 128), "isthing": 0, "id": 7, "trainId": 0, "name": "road"},
+#     {"color": (244, 35, 232), "isthing": 0, "id": 8, "trainId": 1, "name": "sidewalk"},
+#     {"color": (70, 70, 70), "isthing": 0, "id": 11, "trainId": 2, "name": "building"},
+#     {"color": (102, 102, 156), "isthing": 0, "id": 12, "trainId": 3, "name": "wall"},
+#     {"color": (190, 153, 153), "isthing": 0, "id": 13, "trainId": 4, "name": "fence"},
+#     {"color": (153, 153, 153), "isthing": 0, "id": 17, "trainId": 5, "name": "pole"},
+#     {"color": (250, 170, 30), "isthing": 0, "id": 19, "trainId": 6, "name": "traffic light"},
+#     {"color": (220, 220, 0), "isthing": 0, "id": 20, "trainId": 7, "name": "traffic sign"},
+#     {"color": (107, 142, 35), "isthing": 0, "id": 21, "trainId": 8, "name": "vegetation"},
+#     {"color": (152, 251, 152), "isthing": 0, "id": 22, "trainId": 9, "name": "terrain"},
+#     {"color": (70, 130, 180), "isthing": 0, "id": 23, "trainId": 10, "name": "sky"},
+#     {"color": (220, 20, 60), "isthing": 1, "id": 24, "trainId": 11, "name": "person"},
+#     {"color": (255, 0, 0), "isthing": 1, "id": 25, "trainId": 12, "name": "rider"},
+#     {"color": (0, 0, 142), "isthing": 1, "id": 26, "trainId": 13, "name": "car"},
+#     {"color": (0, 0, 70), "isthing": 1, "id": 27, "trainId": 14, "name": "truck"},
+#     {"color": (0, 60, 100), "isthing": 1, "id": 28, "trainId": 15, "name": "bus"},
+#     {"color": (0, 80, 100), "isthing": 1, "id": 31, "trainId": 16, "name": "train"},
+#     {"color": (0, 0, 230), "isthing": 1, "id": 32, "trainId": 17, "name": "motorcycle"},
+#     {"color": (119, 11, 32), "isthing": 1, "id": 33, "trainId": 18, "name": "bicycle"},
+# ]
+
 CITYSCAPES_CATEGORIES = [
-    {"color": (128, 64, 128), "isthing": 0, "id": 7, "trainId": 0, "name": "road"},
+    {"color": (128, 64, 128), "isthing": 0, "id": 7, "trainId": 1, "name": "road"},
     {"color": (244, 35, 232), "isthing": 0, "id": 8, "trainId": 1, "name": "sidewalk"},
+
     {"color": (70, 70, 70), "isthing": 0, "id": 11, "trainId": 2, "name": "building"},
-    {"color": (102, 102, 156), "isthing": 0, "id": 12, "trainId": 3, "name": "wall"},
-    {"color": (190, 153, 153), "isthing": 0, "id": 13, "trainId": 4, "name": "fence"},
-    {"color": (153, 153, 153), "isthing": 0, "id": 17, "trainId": 5, "name": "pole"},
-    {"color": (250, 170, 30), "isthing": 0, "id": 19, "trainId": 6, "name": "traffic light"},
-    {"color": (220, 220, 0), "isthing": 0, "id": 20, "trainId": 7, "name": "traffic sign"},
-    {"color": (107, 142, 35), "isthing": 0, "id": 21, "trainId": 8, "name": "vegetation"},
-    {"color": (152, 251, 152), "isthing": 0, "id": 22, "trainId": 9, "name": "terrain"},
-    {"color": (70, 130, 180), "isthing": 0, "id": 23, "trainId": 10, "name": "sky"},
-    {"color": (220, 20, 60), "isthing": 1, "id": 24, "trainId": 11, "name": "person"},
-    {"color": (255, 0, 0), "isthing": 1, "id": 25, "trainId": 12, "name": "rider"},
-    {"color": (0, 0, 142), "isthing": 1, "id": 26, "trainId": 13, "name": "car"},
-    {"color": (0, 0, 70), "isthing": 1, "id": 27, "trainId": 14, "name": "truck"},
-    {"color": (0, 60, 100), "isthing": 1, "id": 28, "trainId": 15, "name": "bus"},
-    {"color": (0, 80, 100), "isthing": 1, "id": 31, "trainId": 16, "name": "train"},
-    {"color": (0, 0, 230), "isthing": 1, "id": 32, "trainId": 17, "name": "motorcycle"},
-    {"color": (119, 11, 32), "isthing": 1, "id": 33, "trainId": 18, "name": "bicycle"},
+    {"color": (102, 102, 156), "isthing": 0, "id": 12, "trainId": 2, "name": "wall"},
+    {"color": (190, 153, 153), "isthing": 0, "id": 13, "trainId": 2, "name": "fence"},
+
+    {"color": (153, 153, 153), "isthing": 0, "id": 17, "trainId": 3, "name": "pole"},
+    {"color": (250, 170, 30), "isthing": 0, "id": 19, "trainId": 3, "name": "traffic light"},
+    {"color": (220, 220, 0), "isthing": 0, "id": 20, "trainId": 3, "name": "traffic sign"},
+
+    {"color": (107, 142, 35), "isthing": 0, "id": 21, "trainId": 4, "name": "vegetation"},
+    {"color": (152, 251, 152), "isthing": 0, "id": 22, "trainId": 4, "name": "terrain"},
+
+    {"color": (70, 130, 180), "isthing": 0, "id": 23, "trainId": 5, "name": "sky"},
+
+    {"color": (220, 20, 60), "isthing": 1, "id": 24, "trainId": 6, "name": "person"},
+    {"color": (255, 0, 0), "isthing": 1, "id": 25, "trainId": 6, "name": "rider"},
+
+    {"color": (0, 0, 142), "isthing": 1, "id": 26, "trainId": 7, "name": "car"},
+    {"color": (0, 0, 70), "isthing": 1, "id": 27, "trainId": 7, "name": "truck"},
+    {"color": (0, 60, 100), "isthing": 1, "id": 28, "trainId": 7, "name": "bus"},
+    {"color": (0, 80, 100), "isthing": 1, "id": 31, "trainId": 7, "name": "train"},
+    {"color": (0, 0, 230), "isthing": 1, "id": 32, "trainId": 7, "name": "motorcycle"},
+    {"color": (119, 11, 32), "isthing": 1, "id": 33, "trainId": 7, "name": "bicycle"},
 ]
 
 # fmt: off
