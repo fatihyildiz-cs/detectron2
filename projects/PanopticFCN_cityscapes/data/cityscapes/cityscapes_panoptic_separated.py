89b809e18651c07ba0c9f436401c5f96709d6203
Fatih <62537806+fatihyildiz-cs@users.noreply.github.com>
2021-09-20 09:55:23 +0300

    Update cityscapes_panoptic_separated.py

--- projects/PanopticFCN_cityscapes/data/cityscapes/cityscapes_panoptic_separated.py
+++ projects/PanopticFCN_cityscapes/data/cityscapes/cityscapes_panoptic_separated.py
@@ -88,12 +88,12 @@ def load_cityscapes_panoptic(image_dir, gt_dir, gt_json, meta):
     files = get_cityscapes_panoptic_files(image_dir, gt_dir, json_info)
     ret = []
 
+    import pdb; 
+    pdb.set_trace()
     for image_file, label_file, segments_info in files:
         sem_label_file = (
             image_file.replace("leftImg8bit", "gtFine")[:-4]+"_labelTrainIds.png"
         )
-        import pdb; 
-        pdb.set_trace()
         segments_info = [_convert_category_id(x, meta) for x in segments_info]
         ret.append(
             {
